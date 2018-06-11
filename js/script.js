2689bd72a834961f666a38b32d83824eea18a180
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-06-12 01:52:19 +0200

    initialize a project

---
+++ js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+// I start to code a kanban table here 
+var table = {
+    name: 'project',
+    element : '<Node element>'
+    // to method I can add a new column 
+}
+
+
+// every kanban has a columns 
+var column = {
+    id: 12j82da20k,
+    name: 'todo',
+    element: '<Node element>' // for example document.createElement('div')
+    // to method I can add delete a column and createing a new sheets 
+}
+
+// every column has a sheet
+var card = {
+    id: '2kd8s958ka',
+    description: 'Create a kanban app',
+    color: 'green',
+    element: '<Node element>'
+    // to method I can add deleteing a card and moving to another column 
+}
+
+
+
+// when to whole dom tree is loaded the rest of the code will apear 
+document.addEventListener('DOMContentLoaded', function() {
+    // here we will put the code of our application
+});
